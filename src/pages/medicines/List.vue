caa699011c0d3f38402e00c03ece7e8e478bf485
Carlos Arenas <74879959+carlos23ao@users.noreply.github.com>
2020-11-22 19:40:49 -0500

    Medicina

--- src/pages/medicines/List.vue
+++ src/pages/medicines/List.vue
@@ -1,6 +1,35 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="q-pa-md">
+    
+    <q-table
+      title="Medicinas"
+      :data="data"
+      :columns="columns"
+      row-key="id"
+      selection="single"
+      :selected.sync="selected"
+      :filter="filter"
+      :loading="loading"
+      hide-bottom
+    >
+
+      <template v-slot:top>
+        
+        <q-btn color="positive" :disable="loading" label="Añadir Medicina" @click="$router.push({name:'add-medicines'})" />
+        <q-btn class="q-ml-sm" color="warning" :disable="loading" label="Actualizar Medicina" @click="removeRow" />
+        <q-btn class="q-ml-sm" color="negative" :disable="loading" label="Eliminar Medicina" @click="removeRow" />
+        <q-space />
+        <q-input rounded outlined  dense debounce="300" color="primary" v-model="filter">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+    </q-table>
+  </div>
+
   </q-page>
 </template>
 
